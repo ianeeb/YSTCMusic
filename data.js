73c65b8b0804f1f9e7120f3708b651d503f7c0e8
ianeeb <95736356+ianeeb@users.noreply.github.com>
2021-12-10 11:48:40 +0800

    first ver

---
+++ data.js
@@ -0,0 +1,146 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-alice-lee-plaza",
+      "name": "Alice Lee Plaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.570994080168287,
+        "pitch": -0.29121615656706723,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7522538175225844,
+          "pitch": -0.04467785009703995,
+          "rotation": 0,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.204393349612948,
+          "pitch": 0.02061748766637095,
+          "title": "Alice Lee Plaza",
+          "text": "Yong Siew Toh Conservatory of Music"
+        }
+      ]
+    },
+    {
+      "id": "1-conservatory-foyer",
+      "name": "Conservatory Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.7369578397502963,
+        "pitch": 0.06021272500443331,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8909049906710056,
+          "pitch": 0.053166020025877,
+          "rotation": 0,
+          "target": "2-conservatory-lounge"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.751393779042517,
+          "pitch": 0.3311990890202079,
+          "title": "Conservatory Foyer",
+          "text": "Yong Siew Toh Conservatory of Music"
+        }
+      ]
+    },
+    {
+      "id": "2-conservatory-lounge",
+      "name": "Conservatory Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.99295452950118,
+        "pitch": 0.28114877268771465,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.11592800419006,
+          "pitch": 0.022457586293397114,
+          "rotation": 0,
+          "target": "0-alice-lee-plaza"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9627902959180163,
+          "pitch": 0.3559427864145057,
+          "title": "Conservatory Lounge",
+          "text": "Yong Siew Toh Conservatory of Music"
+        }
+      ]
+    }
+  ],
+  "name": "Yong Siew Toh Conservatory of Music",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
